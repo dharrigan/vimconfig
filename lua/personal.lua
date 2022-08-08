c001720ae8cb6dfd529e5078b73e6e31bd03b62d
David Harrigan <dharrigan@gmail.com>
2022-08-08 17:12:48 +0100

    Update lua config

--- lua/personal.lua
+++ lua/personal.lua
@@ -1,4 +1,35 @@
 
+require('telescope').load_extension('coc')
+require('telescope').load_extension('emoji')
 require('telescope').load_extension('env')
 require('telescope').load_extension('fzf')
-require('telescope').load_extension('coc')
+
+require('octo').setup()
+
+local actions = require("telescope.actions")
+
+require('telescope').setup {
+  defaults = {
+    preview = {
+      treesitter = false
+    },
+    mappings = {
+      i = {
+        ["<esc>"] = actions.close,
+        ["<C-j>"] = actions.move_selection_next,
+        ["<C-k>"] = actions.move_selection_previous,
+        ["<C-u>"] = false
+      }
+    },
+    pickers = {
+      find_files = {
+        find_command = { "fd", "--type", "f", "--strip-cwd-prefix" }
+      }
+    },
+    extensions = {
+      fzf = {
+        fuzzy = false
+      }
+    }
+  }
+}
